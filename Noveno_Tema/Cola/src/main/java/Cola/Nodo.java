27a24e0a51712c272f3ed76dcff715ad28676595
Kendall Zuñiga <126116170+KendallZR@users.noreply.github.com>
2023-06-13 21:53:31 -0600

    Add files via upload

---
+++ Noveno_Tema/Cola/src/main/java/Cola/Nodo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cola;
+
+/**
+ *
+ * @author KendallZR
+ */
+public class Nodo {
+    int info; //elemento a guardar 
+    Nodo Siguiente;
+}
