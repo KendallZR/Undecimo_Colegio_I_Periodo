54449c35f4d24244a6077bc69b2e77337932f271
Kendall Zuñiga <126116170+KendallZR@users.noreply.github.com>
2023-06-13 21:42:32 -0600

    Add files via upload

---
+++ Quinto_Tema/Ejemplo_Ejercicio_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo_ejercicio_1;
+
+/**
+ *
+ * @author CTPG
+ */
+public class Ejemplo_Ejercicio_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
